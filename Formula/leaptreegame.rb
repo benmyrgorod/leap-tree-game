1ae3dbd07c4d573f7892ee228157d4497bdd2b99
Ben Myrgorod <65790228+benmyrgorod@users.noreply.github.com>
2026-05-23 20:51:25 -0400

    Update formula

--- Formula/leaptreegame.rb
+++ Formula/leaptreegame.rb
@@ -1,7 +1,7 @@
-class Mytool < Formula
+class Leaptreegame < Formula
   include Language::Python::Virtualenv
 
-  desc "Awesome CLI tool"
+  desc "Leap Tree Game is an experimental AI storytelling game"
   homepage "https://github.com/benmyrgorod/homebrew-leaptreegame"
   url "b9f93456d101211c0e31b96a11359d26977712d702b7793eacf50bd391552c98"
   sha256 "PUT_REAL_SHA256_HERE"
